0b3170b658a26fb1648ba909e7dee9b9016cffe8
Alex Irizarry <irizarryam5@gmail.com>
2019-02-18 17:01:38 -0500

    debugging

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//app.use(express.static(path.join(__dirname, 'public')));
-//
-//let index = path.resolve(__dirname + '/index.html');
-//
-//app.get('/', (req, res) => {
-//  res.render(index);
-//});
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.listen(80, () => {
-//  console.log("listening. . .");
-//});
+let index = path.resolve(__dirname + '/index.html');
+
+app.get('/', (req, res) => {
+  res.render(index);
+})
+app.listen(8080, () => {
+  console.log("listening. . .");
+});
